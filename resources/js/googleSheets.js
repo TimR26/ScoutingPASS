8ddd96b059d3772589140d4722b7d3428ba2c842
TimR26 <95718018+TimR26@users.noreply.github.com>
2023-06-26 15:48:20 -0400

    Update googleSheets.js
    vdvxdv

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -1,5 +1,5 @@
 function setUpGoogleSheets() {
-    const scriptURL = '<SCRIPT URL>'
+    const scriptURL = 'https://script.google.com/macros/s/AKfycby1pC-CkvYyhpZ3hWbojYweecl2X8eNIctZARLROZChyo8APLOohvBjTccQfKbjy3Gv/exec'
     const form = document.querySelector('#scoutingForm')
     const btn = document.querySelector('#submit')
  
